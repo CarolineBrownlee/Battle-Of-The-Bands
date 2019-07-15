8883311c3ea86b15ae7b241a73b067028c35260e
Caroline Brownlee <brownleecaroline@gmail.com>
2019-07-15 14:54:32 -0500

    finished battle of the bands exercise

---
+++ bands.js
@@ -0,0 +1,11 @@
+let bandNumber = 0
+
+const takeNumber = function (bandName) {
+    bandNumber++;
+    console.log(bandNumber + "." + " " + bandName);
+}
+
+const scum = takeNumber("Galactic Scum")
+
+
+const under = takeNumber("Underdogs")
